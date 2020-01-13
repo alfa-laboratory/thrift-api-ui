1d20dd6296578054ba041b8d47f4a5e21ec6cf4c
Andrii Bondar <andrii.bondar@theopentag.com>
2020-01-13 14:43:27 +0200

    Add Content-Type header to each request

--- src/renderer/thrift/performRequest.ts
+++ src/renderer/thrift/performRequest.ts
@@ -29,6 +29,9 @@ export async function performRequest(
         uri: endpoint,
         body: buffer,
         encoding: null,
+        headers: {
+            'Content-Type': 'application/x-thrift',
+        },
         timeout,
         proxy
     });
